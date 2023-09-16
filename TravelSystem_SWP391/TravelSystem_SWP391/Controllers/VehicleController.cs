7ab8c7de53833aea74220cd2e8f2dd9788346c2e
namlxhe153241 <namlxhe153241@fpt.edu.vn>
2023-09-16 23:31:08 +0700

    Add View List Vehicle

---
+++ TravelSystem_SWP391/TravelSystem_SWP391/Controllers/VehicleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelSystem_SWP391.Controllers
+{
+    public class VehicleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ViewListVehicle()
+        {
+            return View();
+        }
+    }
+}
