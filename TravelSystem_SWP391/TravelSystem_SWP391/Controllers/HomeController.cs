7fb1b9e472a5498a71d8d15bec78f3fd99d3546b
namlxhe153241 <namlxhe153241@fpt.edu.vn>
2023-09-18 15:49:17 +0700

    big commit screen

---
+++ TravelSystem_SWP391/TravelSystem_SWP391/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelSystem_SWP391.Controllers
+{
+	public class HomeController : Controller
+	{
+
+		public IActionResult tours()
+		{
+
+			return View();
+		}
+		public IActionResult index()
+		{
+			
+			return View();
+		}
+		public IActionResult blog()
+		{
+			
+			return View();
+		}
+		public IActionResult contacts()
+		{
+			return View();
+		}
+		public IActionResult about()
+		{
+			return View();
+		}
+		public IActionResult gallery()
+		{
+			return View();
+		}
+	}
+}
--- TravelSystem_SWP391/TravelSystem_SWP391/Program.cs
+++ TravelSystem_SWP391/TravelSystem_SWP391/Program.cs
@@ -1,17 +1,26 @@
 namespace TravelSystem_SWP391
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
 			var builder = WebApplication.CreateBuilder(args);
 			builder.Services.AddControllersWithViews();
 			var app = builder.Build();
 			app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Login}/{action=Login}/{id?}"
+	pattern: "{controller=Home}/{action=index}/{id?}"
+
 	);
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			//app.MapRazorPages();
 			app.Run();
-        }
-    }
+		}
+	}
 }
