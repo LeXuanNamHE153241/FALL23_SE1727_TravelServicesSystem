4d9ba882e2bc12068451f54975273f6e6d1cc204
linhpthe140842 <linhpthe140842@fpt.edu.vn>
2023-09-20 20:17:17 +0700

    Commit StaffController

---
+++ TravelSystem_SWP391/TravelSystem_SWP391/Controllers/StaffController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelSystem_SWP391.DAO_Context;
+using TravelSystem_SWP391.Models;
+
+namespace TravelSystem_SWP391.Controllers
+{
+    public class StaffController : Controller
+    {
+        DAO dal = new DAO();
+
+        [HttpGet]
+        public ActionResult List()
+        {
+            List<staff> staffs = dal.GetStaffs();
+            return View(staffs);
+        }
+
+    }
+
+}
