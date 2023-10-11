b9e241d43ef897ef9edff2798af9a8d8dbbd11f7
namlxhe153241 <namlxhe153241@fpt.edu.vn>
2023-10-11 14:41:02 +0700

    add restaurant

---
+++ TravelSystem_SWP391/TravelSystem_SWP391/Controllers/RestaurantController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelSystem_SWP391.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        public IActionResult ViewListRes()
+        {
+            return View();
+        }
+    }
+}
--- TravelSystem_SWP391/TravelSystem_SWP391/Models/traveltestContext.cs
+++ TravelSystem_SWP391/TravelSystem_SWP391/Models/traveltestContext.cs
@@ -30,7 +30,7 @@ namespace TravelSystem_SWP391.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server =DESKTOP-90J3IP6; database = traveltest;uid=sa;pwd=sa;");
+                optionsBuilder.UseSqlServer("server =DESKTOP-OAU09UE\\SQLEXPRESS; database = traveltest;uid=nam29;pwd=29;");
             }
         }
 
