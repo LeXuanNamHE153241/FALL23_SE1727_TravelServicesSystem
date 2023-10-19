ac293cc599e8465544c9fe839a41e5b3ad362325
namlxhe153241 <namlxhe153241@fpt.edu.vn>
2023-10-19 12:47:30 +0700

    commit 19/10

--- TravelSystem_SWP391/TravelSystem_SWP391/Models/traveltestContext.cs
+++ TravelSystem_SWP391/TravelSystem_SWP391/Models/traveltestContext.cs
@@ -32,7 +32,10 @@ namespace TravelSystem_SWP391.Models
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
 
 
+
                 optionsBuilder.UseSqlServer("server =DESKTOP-OAU09UE\\SQLEXPRESS; database = traveltest;uid=nam29;pwd=29;");
+
+
 
 
 
@@ -342,6 +345,10 @@ namespace TravelSystem_SWP391.Models
                     .HasMaxLength(50)
                     .HasColumnName("description");
 
+                entity.Property(e => e.EmailUser)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("Email_User");
 
                 entity.Property(e => e.EmailUser)
                     .HasMaxLength(50)
